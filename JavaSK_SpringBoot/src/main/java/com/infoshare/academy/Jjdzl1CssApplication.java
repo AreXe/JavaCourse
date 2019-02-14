47cb69ed5356e16bc161891fc3535c84e51cd08b
arexe <arexepro@gmail.com>
2019-02-14 17:29:36 +0100

    JavaSK_SpringBoot lesson 1 - initialize Spring Boot project with Hello World

---
+++ JavaSK_SpringBoot/src/main/java/com/infoshare/academy/Jjdzl1CssApplication.java
@@ -0,0 +1,14 @@
+package com.infoshare.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jjdzl1CssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jjdzl1CssApplication.class, args);
+	}
+
+}
+
